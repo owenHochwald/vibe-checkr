683155b8f045eb8949c13e17273644d36247eb5f
owenHochwald <owenhochwald@gmail.com>
2025-04-02 21:40:13 -0700

    add initial template for code reviewer command

--- vibe-checker/src/extension.ts
+++ vibe-checker/src/extension.ts
@@ -3,13 +3,15 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "vibe-checker" is now active!');
 
-    const disposable = vscode.commands.registerCommand('vibe-checker.helloWorld', () => {
-
+    const disposable = vscode.commands.registerCommand('vibe-checker.reviewCode',
+        async () => {
             vscode.window.showInformationMessage(`Hello World`);
+            // TODO: implement call deepseek.ts
     
     });
 
     context.subscriptions.push(disposable);
+
 }
 
 export function deactivate() {}
